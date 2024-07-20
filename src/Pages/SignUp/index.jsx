9dd0881f7b48e64b76b761b5c0001810991f0799
PalatronDead <palatron33@outlook.com>
2024-07-19 18:10:15 -0700

    Completed Layout of user Views!

--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect } from "react";
 import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "../../Context/useLocalStorage";
 
 function SignUp() {
-  const { userData, setUserData, setIsSignOut } =
+  const { userData, setUserData, setIsSignOut, isSignOut } =
     useContext(ShoppingCartContext);
   const { item: dataUser, saveItem: saveDataUser } = useLocalStorage(
     "User",
@@ -15,6 +15,8 @@ function SignUp() {
     "Sign-out",
     true,
   );
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,19 +28,32 @@ function SignUp() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (userData.email === dataUser.email) {
-      console.log("Uppps, yikes, si tienes cuenta registrada");
+    if (isSignOut) {
+      if (
+        userData.name === dataUser.name &&
+        userData.email === dataUser.email &&
+        userData.password === dataUser.password
+      ) {
+        console.log("State user Data: ", userData);
+        console.log("LocalStorage User Data: ", dataUser);
+        console.log("Uppps, yikes, si tienes cuenta registrada");
+      } else {
+        setIsSignOut(false);
+        saveSignOutData(false);
+        saveDataUser(userData);
+        console.log("State user Data: ", userData);
+        console.log("LocalStorage User Data: ", dataUser);
+      }
     } else {
       setIsSignOut(false);
       saveSignOutData(false);
       saveDataUser(userData);
-      console.log("State user Data: ", userData);
-      console.log("LocalStorage User Data: ", dataUser);
+      navigate("/");
     }
   };
 
   const renderView = () => {
-    if (isSignUp) {
+    if (isSignOut) {
       return (
         <>
           <div className="mb-4 flex w-80 items-center justify-center">
@@ -90,7 +105,7 @@ function SignUp() {
       return (
         <>
           <div className="mb-4 flex w-80 items-center justify-center">
-            <h1 className="text-xl font-medium">Sign Up</h1>
+            <h1 className="text-xl font-medium">Edit Account</h1>
           </div>
           <div className="w-80">
             <form onSubmit={handleSubmit}>
@@ -118,7 +133,7 @@ function SignUp() {
                 <label htmlFor="password">Your password</label>
                 <input
                   className="w-80 rounded-lg border border-black p-2 focus:outline-none"
-                  type="password"
+                  type="text"
                   name="password"
                   value={userData.password}
                   onChange={handleChange}
@@ -139,51 +154,7 @@ function SignUp() {
 
   return (
     <>
-      <Layout>
-        <div className="mb-4 flex w-80 items-center justify-center">
-          <h1 className="text-xl font-medium">Sign Up</h1>
-        </div>
-        <div className="w-80">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="name">Your name</label>
-              <input
-                className="w-80 rounded-lg border border-black p-2 focus:outline-none"
-                type="text"
-                name="name"
-                value={userData.name}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email">Your email</label>
-              <input
-                className="w-80 rounded-lg border border-black p-2 focus:outline-none"
-                type="email"
-                name="email"
-                value={userData.email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password">Your password</label>
-              <input
-                className="w-80 rounded-lg border border-black p-2 focus:outline-none"
-                type="password"
-                name="password"
-                value={userData.password}
-                onChange={handleChange}
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full rounded-lg border border-black bg-black py-2 text-white shadow-md hover:shadow-lg"
-            >
-              Create
-            </button>
-          </form>
-        </div>
-      </Layout>
+      <Layout>{renderView()}</Layout>
     </>
   );
 }
